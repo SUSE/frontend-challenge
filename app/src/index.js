7825b6e3ae3ba06fba27cfe6af443cd4696e399f
Víctor Montalà <victormontala@gmail.com>
2018-01-31 22:40:15 +0100

    Add app dependencies, restructure index file and remove main css file

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'babel-polyfill';
+import 'babel-polyfill'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+registerServiceWorker()
