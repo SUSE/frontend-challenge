159dbc2910b6ffc8622b9a902ac2ec41d37010c5
Víctor Montalà <victormontala@gmail.com>
2018-02-11 19:40:25 +0100

    Use MachineDetail model on MachineDetail component

--- app/src/MachineDetail/index.js
+++ app/src/MachineDetail/index.js
@@ -4,140 +4,25 @@ import Section from './Section'
 
 class MachineDetail extends Component {
   render() {
-    let machine = [
-      {
-        'items': [
-          {
-            'info': 'This machine is virtualised',
-            'key': 'virtualised'
-          },
-          {
-            'description': '64 bit, 4 cores',
-            'key': 'CPU',
-            'value': 'Intel(R) Core(TM) i7-4930K CPU @ 3.40GHz'
-          },
-          {
-            'key': 'GPU',
-            'value': 'VirtualBox Graphics Adapter'
-          },
-          {
-            'key': 'RAM',
-            'value': '2 GB'
-          }
-        ],
-        'name': 'Machine'
-      },
-      {
-        'items': [
-          {
-            'key': 'Version',
-            'value': 'VirtualBox'
-          },
-          {
-            'key': 'Release',
-            'value': '12/01/2006'
-          }
-        ],
-        'name': 'BIOS'
-      },
-      {
-        'items': [
-          {
-            'key': 'Kernel',
-            'value': 'Linux 3.13.0-52-generic'
-          },
-          {
-            'description': 'trusty',
-            'key': 'Version',
-            'value': 'Ubuntu 14.04.2 LTS'
-          },
-          {
-            'key': 'Family',
-            'value': 'Debian'
-          },
-          {
-            'key': 'Architecture',
-            'value': '64 bit'
-          }
-        ],
-        'name': 'Operating System'
-      },
-      {
-        'items': [
-          {
-            'key': 'Encoding',
-            'value': 'UTF-8'
-          },
-          {
-            'code': 'en_US',
-            'key': 'Language'
-          },
-          {
-            'code': '/bin/bash',
-            'key': 'Shell'
-          },
-          {
-            'code': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-            'key': 'Path'
-          }
-        ],
-        'name': 'Session'
-      },
-      {
-        'items': [
-          {
-            'code': '192.168.56.102',
-            'key': 'Master'
-          }
-        ],
-        'list': [
-          {
-            'items': [
-              {
-                'code': '10.0.2.15',
-                'key': 'IPV4'
-              },
-              {
-                'code': 'fe80::a00:27ff:fea8:99a8',
-                'key': 'IPV6'
-              },
-              {
-                'code': '08:00:27:a8:99:a8',
-                'key': 'MAC'
-              }
-            ],
-            'name': 'eth0'
-          },
-          {
-            'items': [
-              {
-                'code': '192.168.56.103',
-                'key': 'IPV4'
-              },
-              {
-                'code': 'fe80::a00:27ff:fe9b:a023',
-                'key': 'IPV6'
-              },
-              {
-                'code': '08:00:27:9b:a0:23',
-                'key': 'MAC'
-              }
-            ],
-            'name': 'eth1'
-          }
-        ],
-        'name': 'Network'
-      }
-    ]
+    let machine = this.props.machine,
+        sections = machine.getViewSections()
 
     return (
-      <article className="col d-none d-sm-block flex-column border-left p-3">
+      <article>
         <header>
           <h4 className="card-title font-weight-normal mb-4">
-            <span title="Machine Name">suma-ref31-cli-sles12sp3-1</span> <span className="small">(<span className="font-weight-light text-muted" title="Machine ID">a2a7915642f3efeec4837a015a679bc1</span>)</span>
+            <span title="Machine Name">{ machine.name }</span>&nbsp;
+            <span className="small">
+              (
+                <span
+                  className="font-weight-light text-muted"
+                  title="Machine ID"
+                >{ machine.public_id }</span>
+              )
+            </span>
           </h4>
         </header>
-        {machine.map((section, index) => {
+        {sections.map((section, index) => {
           return <Section key={ index } section={ section }></Section>
         })}
       </article>
--- app/src/common/models/MachineDetail.js
+++ app/src/common/models/MachineDetail.js
@@ -1,4 +1,165 @@
 class MachineDetail {
+  constructor(data) {
+    this.id = data.id
+    this.public_id = data.machine_id
+    this.name = data.nodename
+
+    this.bios = [
+      {
+        'key': 'Version',
+        'value': data.biosversion,
+      },
+      {
+        'key': 'Release',
+        'value': data.biosreleasedate
+      }
+    ]
+
+    this.machine = []
+
+    if(data.virtual) {
+      this.machine.push(
+        {
+          'key': 'virtualised',
+          'info': 'This machine is virtualised'
+        }
+      )
+    }
+
+    this.machine.push(
+      {
+        'key': 'CPU',
+        'value': data.cpu_model,
+        'description': `${ data.cpuarch.match('64') ? '64' : '32' } bit, ${ data.num_cpus } cores`
+      },
+      {
+        'key': 'GPU',
+        'value': data.gpus[0].model,
+      },
+      {
+        'key': 'RAM',
+        'value': `${ Math.floor(data.mem_total / 1000) } GB`
+      }
+    )
+
+    this.os = [
+      {
+        'key': 'Kernel',
+        'value': `${ data.kernel } ${ data.kernelrelease }`,
+      },
+      {
+        'key': 'Version',
+        'value': data.lsb_distrib_description,
+        'description': data.oscodename
+      },
+      {
+        'key': 'Family',
+        'value': data.os_family,
+      },
+      {
+        'key': 'Architecture',
+        'value': `${ data.osarch.match('64') ? '64' : '32' } bit`
+      }
+    ]
+
+    this.session = [
+      {
+        'key': 'Encoding',
+        'value': data.locale_info.defaultencoding
+      },
+      {
+        'key': 'Language',
+        'code': data.locale_info.defaultlanguage
+      },
+      {
+        'key': 'Shell',
+        'code': data.shell
+      },
+      {
+        'key': 'Path',
+        'code': data.path
+      }
+    ]
+
+    this.network = {
+      global: [
+        {
+          'key': 'Master',
+          'code': '192.168.56.102'
+        }
+      ],
+      interfaces: this.getNetworkInterfaces(data)
+    }
+  }
+
+  getNetworkInterfaces(data) {
+    let reference = [
+          {
+            'name': 'IPV4',
+            'key': 'ip4_interfaces'
+          },
+          {
+            'name': 'IPV6',
+            'key': 'ip6_interfaces'
+          },
+          {
+            'name': 'MAC',
+            'key': 'hwaddr_interfaces'
+          }
+        ],
+        interfaceList = []
+
+    reference.forEach((item) => {
+      for(let key in data[item.key]) {
+        if(key !== 'lo') {
+          let interfaceItem = interfaceList.find((item) => item.name === key),
+              value = data[item.key][key]
+
+          if(!interfaceItem) {
+            interfaceItem = {
+              name: key,
+              items: []
+            }
+
+            interfaceList.push(interfaceItem)
+          }
+
+          interfaceItem.items.push({
+            'key': item.name,
+            'code': typeof value === 'string' ? value : value[0]
+          })
+        }
+      }
+    })
+
+    return interfaceList
+  }
+
+  getViewSections() {
+    return [
+      {
+        'name': 'Machine',
+        'items': this.machine
+      },
+      {
+        'name': 'BIOS',
+        'items': this.bios
+      },
+      {
+        'name': 'Operating System',
+        'items': this.os
+      },
+      {
+        'name': 'Session',
+        'items': this.session
+      },
+      {
+        'name': 'Network',
+        'items': this.network.global,
+        'list': this.network.interfaces
+      }
+    ]
+  }
 }
 
 export default MachineDetail
